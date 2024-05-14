99d9c97ea0fc319719c8a640fe36e3d04d48b7a5
Victor Assunção <victor.a.schumann@gmail.com>
2024-05-14 15:30:28 -0300

    exc 1 lab semana 5 e 6 classe cliente

--- Semanas 5 e 6/LAB exc1/Cliente.java
+++ Semanas 5 e 6/LAB exc1/Cliente.java
@@ -1,2 +1,63 @@
-public class Cliente{
+public class Cliente {
+    private String nome;
+    private String email;
+    private char sexo;
+    private int idade;
+    private String senha;
+    
+    public Cliente(String nome, String email, char sexo, int idade, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.senha = senha;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public char getSexo() {
+        return sexo;
+    }
+    
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public void imprimeInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Idade: " + idade);
+        System.out.println("Senha:" + senha);
+    }
 }
